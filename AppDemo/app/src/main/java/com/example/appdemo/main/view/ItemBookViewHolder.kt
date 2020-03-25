551d5a8f9a15a710b8d517e380948dbf4e9bd4ac
Jhonata Avila <jo.avila@gmail.com>
2020-03-25 16:21:40 -0300

    refac(ItemBookViewHolder): passando cod para padrao kt.

--- AppDemo/app/src/main/java/com/example/appdemo/main/view/ItemBookViewHolder.kt
+++ AppDemo/app/src/main/java/com/example/appdemo/main/view/ItemBookViewHolder.kt
@@ -9,9 +9,8 @@ import kotlinx.android.synthetic.main.item_book.view.*
 public class ItemBookViewHolder (itemView: View): RecyclerView.ViewHolder(itemView) {
 
     fun bind(book: Book) {
-        itemView.text_title.setText(book.name)
-        itemView.text_author.setText(book.author)
+        itemView.text_title.text = book.name
+        itemView.text_author.text = book.author
         Glide.with(itemView.context).load(book.thumb).into(itemView.image_cover_reading)
-
     }
 }
